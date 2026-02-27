ed0e91aea70e0b41c584523b174360ac02157673
AlazOflaz06 <berkeay19078@gmail.com>
2026-02-27 20:53:39 +0300

    Update login success message
    Shortened the login success message for clarity.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function checkPassword(){
     var message = document.getElementById("loginMessage");
 
     if(pass === "nisa"){
-        message.innerHTML = "Girişiniz onaylanmıştır Nisa Hanım doğum gününüz kutlu olsun müziği dinlemek için sesi açınız";
+        message.innerHTML = "Girişiniz onaylanmıştır Nisa Hanım doğum gününüz kutlu olsun.";
         setTimeout(function(){
             window.location.href = "anasayfa.html";
         },2000);
